4f9902e9d35d74d651d007c5d1455332c587a132
Игорь Герасимов <g-igor92@mail.ru>
2022-09-15 20:17:04 +0500

    Домашнее задание к Уроку 5

--- Program.cs
+++ Program.cs
@@ -1,88 +1,92 @@
-﻿int Zadacha25(int number, int degree) {
-
-    // Используя определение степени числа, напишите цикл, который принимает на вход два натуральных числа (A и B) и возводит число A в степень B.
-    int result = number;
-
-    for (int i = 1; i < degree; i++) {
-        result *= number;
-    }
-
-    return result;
-}
-
-int Zadacha27(int number) {
-
-    // Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
-    int sum = 0;
-    while (number > 0) {
-        sum = sum+number%10;
-        number = number/10;
-    }
-
-    return sum;
-}
-
-void Zadacha29() {
-    int length = 8;
-    int[] arr = new int[length];
-    Random rand = new Random();
-
-    
-    Console.WriteLine("Выведем стартовый массив:");
-    for (int i = 0; i < arr.Length; i++) {
-        arr[i] = rand.Next(-100, 101);
-        Console.Write($"{arr[i]}, ");
-    }
+﻿void Zadacha34() {
+    Console.WriteLine("Задача 34. Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве");
+    Console.WriteLine("Формируем массив:");
+    int[] array = new int[10];
+    FillArray(array, 100, 1000);
+    PrintArray(array);
+    CountEven(array);
     Console.WriteLine();
+}
 
-    Console.WriteLine("Выведем отсортированный массив:");
-    int min = arr[0];
-    int bar = arr[0];
-    int index = 0;
+void Zadacha36() {
+    Console.WriteLine("Задача 36. Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.");
+    Console.WriteLine("Формируем массив:");
+    int[] array = new int[10];
+    FillArray(array, 0, 10);
+    PrintArray(array);
+    SumUneven(array);
+    Console.WriteLine();
+}
 
-    for (int i = 0; i < arr.Length - 1; i++) {
-        min = arr[i];
-        bar = arr[i];
-        index = i;
-        for (int j = i + 1; j < arr.Length; j++) {
-            if (Math.Abs(arr[j]) < Math.Abs(min)) {
-                min = arr[j];
-                index = j;
-            }
+void Zadacha38() {
+    Console.WriteLine("Задача 38. Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.");
+    Console.WriteLine("Формируем массив:");
+    double[] array = new double[10];
+    FillArrayDouble(array);
+    PrintArrayDouble(array);
+    Sum(array);
+    Console.WriteLine();
+}
+
+void FillArrayDouble(double[] arr) {
+    // Массив заполняется числами double от -10 до 10. Параметр k определяет будет элемент массива со знаком "+" или "-", поскольку NextDouble выдает только положительные числа.
+    Random rand = new Random();
+    int k;
+    for (int i = 0; i < arr.Length; i++) {
+        k = 0;
+        while (k == 0) {
+            k = rand.Next(-1, 2);
         }
-        arr[i] = min;
-        arr[index] = bar;
+        arr[i] = rand.NextDouble()*10*k;
     }
+}
 
+void Sum(double[] arr){
+    double substraction = arr[0] + arr[arr.Length-1];
+    Console.WriteLine($"Cумма первого и последнего элементов массива = {Math.Round(substraction, 2)}");
+}
+
+void PrintArrayDouble(double[] arr) {
+    for (int i = 0; i < arr.Length; i++) {
+        Console.Write($"{Math.Round(arr[i], 2)}; ");
+    }
+    Console.Write("\b\b");
+    Console.WriteLine();
+}
+
+void SumUneven(int[] arr) {
+    int sum = 0;
+    for (int i = 1; i < arr.Length; i = i+2) {
+        sum += arr[i];
+    }
+    Console.WriteLine($"Сумма чисел на нечетных позициях массива = {sum}");
+}
+
+void CountEven(int[] arr) {
+    int counter = 0;
+    for (int i = 0; i < arr.Length; i++) {
+        if (arr[i]%2 == 0){
+            counter +=1;
+        }
+    }
+    Console.WriteLine($"В заданном массиве четных чисел = {counter}");
+}
+
+void FillArray(int[] arr, int min = -100, int max = 101) {
+    Random rand = new Random();
+    for (int i = 0; i < arr.Length; i++) {
+        arr[i] = rand.Next(min, max);
+    }
+}
+
+void PrintArray(int[] arr) {
     for (int i = 0; i < arr.Length; i++) {
         Console.Write($"{arr[i]}, ");
     }
-
+    Console.Write("\b\b");
+    Console.WriteLine();
 }
 
-
-Console.WriteLine("Начинаем выполнение задачи №25 - Используя определение степени числа, напишите цикл, который принимает на вход два натуральных числа (A и B) и возводит число A в степень B.");
-Console.WriteLine("Введите число A");
-int number = Convert.ToInt32(Console.ReadLine());
-while (number <=0) {
-    Console.WriteLine("Ошибка! Вы должны ввести натуральное число");
-    number = Convert.ToInt32(Console.ReadLine());
-}
-Console.WriteLine("Введите степень B");
-int degree = Convert.ToInt32(Console.ReadLine());
-while (degree <=0) {
-    Console.WriteLine("Ошибка! Вы должны ввести натуральное число");
-    degree = Convert.ToInt32(Console.ReadLine());
-}
-Console.WriteLine("Ответ на задачу №25 = " + Zadacha25(number, degree));
-Console.WriteLine();
-
-Console.WriteLine("Начинаем выполенение задачи №27 - Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.");
-Console.WriteLine("Введите число");
-int number1 = Convert.ToInt32(Console.ReadLine());
-Zadacha27(number1);
-Console.WriteLine("Ответ на задачу №27 = " + Zadacha27(number1));
-Console.WriteLine();
-
-Console.WriteLine("Начинаем выполенение задачи №29 - Напишите программу, которая задаёт массив из 8 случайных целых чисел и выводит отсортированный по модулю массив.");
-Zadacha29();
+Zadacha34();
+Zadacha36();
+Zadacha38();
