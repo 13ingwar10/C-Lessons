e252da48a79735e3055bd7edb18e76a456e1ed6d
Игорь Герасимов <g-igor92@mail.ru>
2022-09-16 16:18:42 +0500

    Домашнее задание к Уроку 6

--- Program.cs
+++ Program.cs
@@ -1,103 +1,64 @@
-﻿void Zadacha34() {
-    Console.WriteLine("Задача 34. Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве");
-    Console.WriteLine("Формируем массив:");
-    int[] array = new int[10];
-    FillArray(array, 100, 1000);
-    PrintArray(array);
-    CountEven(array);
-    Console.WriteLine();
+﻿void Zadacha41() {
+    Console.WriteLine("Задача 41. Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.");
+    int[] arr = CreateArray();
+    PrintArray(arr);
+    Console.WriteLine($"В вашем массиве {CountPositive(arr)} чисел больше 0");
 }
 
-void Zadacha36() {
-    Console.WriteLine("Задача 36. Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.");
-    Console.WriteLine("Формируем массив:");
-    int[] array = new int[10];
-    FillArray(array, 0, 10);
-    PrintArray(array);
-    SumUneven(array);
-    Console.WriteLine();
+void Zadacha43() {
+    Console.WriteLine("Задача 43. Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.");
+    Console.WriteLine("Задайте через Enter коэффициенты b и k для первой прямой");
+    double[] FirstStraight = CreateStraights();
+        Console.WriteLine("Задайте через Enter коэффициенты b и k для второй прямой");
+    double[] SecondStraight = CreateStraights();
+    SolveEquation(FirstStraight, SecondStraight);
 }
 
-void Zadacha38() {
-    Console.WriteLine("Задача 38. Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.");
-    Console.WriteLine("Формируем массив:");
-    double[] array = new double[10];
-    FillArrayDouble(array);
-    PrintArrayDouble(array);
-    Sum(array);
-    Console.WriteLine();
+void SolveEquation(double[] arr1, double[] arr2){
+    double[] ArrayXY = new double[2];
+    ArrayXY[0] = (arr2[1] - arr1[1]) / (arr1[0] - arr2[0]);
+    ArrayXY[1] = arr1[0]*ArrayXY[0]+arr1[1];
+    Console.WriteLine($"Прямые пересекутся в точке:({ArrayXY[0]};{ArrayXY[1]})");
 }
 
-void FillArrayDouble(double[] arr) {
-    // Массив заполняется числами double от -10 до 10. Параметр k определяет будет элемент массива со знаком "+" или "-", поскольку NextDouble выдает только положительные числа.
-    Random rand = new Random();
-    int k;
-    for (int i = 0; i < arr.Length; i++) {
-        k = 0;
-        while (k == 0) {
-            k = rand.Next(-1, 2);
-        }
-        arr[i] = rand.NextDouble()*10*k;
+double[] CreateStraights() {
+    double[] arr = new double[2];
+    for (int i = 2; i > 0; i--) {
+        arr[i-1] = Convert.ToDouble(Console.ReadLine());
     }
+    return arr;
 }
 
-void Sum(double[] arr){
-    //double substraction = arr[0] + arr[arr.Length-1];
-    double substraction = 0;
-    double min = arr[0];
-    double max = arr[0];
-    for (int i = 1; i < arr.Length; i++) {
-        if (arr[i] < min) {
-            min = arr[i];
-        } else if (arr[i] > max) {
-            max = arr[i];
-        }
+int[] CreateArray(){
+    Console.WriteLine("Сколько чисел вы хотите ввести (укажите число не больше 10)?");
+    int size = Convert.ToInt32(Console.ReadLine());
+    while (size > 10) {
+        Console.WriteLine("Ошибка! Введите число не больше 10:");
+            size = Convert.ToInt32(Console.ReadLine());
     }
-    substraction = max-min;
-    Console.WriteLine($"Разница между максимальным и минимальным элементом масссива = {Math.Round(substraction, 2)}");
-}
-
-void PrintArrayDouble(double[] arr) {
-    for (int i = 0; i < arr.Length; i++) {
-        Console.Write($"{Math.Round(arr[i], 2)}; ");
+    int[] arr = new int[size];
+    Console.WriteLine($"Отлично! Введите через Enter {size} чисел");
+    for (int i = 0; i < size; i++) {
+        arr[i] = Convert.ToInt32(Console.ReadLine());
     }
-    Console.Write("\b\b");
-    Console.WriteLine();
+    Console.WriteLine($"Готово! Массив из {size} чисел сформирован:");
+    return arr;
 }
 
-void SumUneven(int[] arr) {
-    int sum = 0;
-    for (int i = 1; i < arr.Length; i = i+2) {
-        sum += arr[i];
-    }
-    Console.WriteLine($"Сумма чисел на нечетных позициях массива = {sum}");
-}
-
-void CountEven(int[] arr) {
+int CountPositive(int[] array) {
     int counter = 0;
-    for (int i = 0; i < arr.Length; i++) {
-        if (arr[i]%2 == 0){
-            counter +=1;
-        }
+    for (int i = 0; i < array.Length; i++) {
+        if (array[i] > 0) counter++;
     }
-    Console.WriteLine($"В заданном массиве четных чисел = {counter}");
-}
-
-void FillArray(int[] arr, int min = -100, int max = 101) {
-    Random rand = new Random();
-    for (int i = 0; i < arr.Length; i++) {
-        arr[i] = rand.Next(min, max);
-    }
+    return counter;
 }
 
 void PrintArray(int[] arr) {
     for (int i = 0; i < arr.Length; i++) {
-        Console.Write($"{arr[i]}, ");
+        Console.Write($"{arr[i]}  ");
     }
-    Console.Write("\b\b");
     Console.WriteLine();
 }
 
-Zadacha34();
-Zadacha36();
-Zadacha38();
+Zadacha41();
+Zadacha43();
