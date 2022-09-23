5dc8dc959ee58ebbf96fa2d3c96eccc1ff79ae4a
Игорь Герасимов <g-igor92@mail.ru>
2022-09-24 00:57:36 +0500

    Домашнее задание к Уроку 9

--- Program.cs
+++ Program.cs
@@ -1,159 +1,84 @@
-﻿void Zadacha54() {
-    Console.WriteLine("Задача 54. Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.");
-    var rand = new Random();
-    int[,] array = new int[rand.Next(2, 10),rand.Next(2, 10)];
-    FillArray(array);
-    PrintArrayInt(array);
-    Console.WriteLine("Сортируем элементы строк max -> min");  
-    SortMaxToMin(array);
-    PrintArrayInt(array);
+﻿void Zadacha64() {
+    Console.WriteLine("Задача 64. Задайте значения M и N. Напишите рекурсионный метод, который выведет все натуральные числа кратные 3-ём в промежутке от M до N.");
+    Console.WriteLine("Введите число М от 0 до 100:");
+    int m = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите число N от 0 до 100:");
+    int n = Convert.ToInt32(Console.ReadLine());
+
+    while (n > m) {
+        Console.WriteLine("Ошибка! Число N < M");
+        Console.WriteLine("Введите число М от 0 до 100:");
+        m = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите число N от 0 до 100:");
+        n = Convert.ToInt32(Console.ReadLine());       
+    }
+    
+    PrintNumbers(m,n);
     Console.WriteLine();   
 }
 
-void Zadacha56() {
-    Console.WriteLine("Задача 56. Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.");
-    var rand = new Random();
-    int[,] array = new int[rand.Next(2, 10),rand.Next(2, 10)]; 
-    FillArray(array);
-    Console.WriteLine("Был сформирован следующий массив:");
-    PrintArrayInt(array);   
-    FindLessLine(FindSumsOfLines(array));
-    Console.WriteLine();  
-}
+void Zadacha66() {
+    Console.WriteLine("Задача 66. Задайте значения M и N. Напишите рекурсионный метод, который найдёт сумму натуральных элементов в промежутке от M до N.");
+    Console.WriteLine("Введите число М от 0 до 100:");
+    int m = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите число N от 0 до 100:");
+    int n = Convert.ToInt32(Console.ReadLine());
 
-void Zadacha58() {
-    Console.WriteLine("Задача 58. Заполните спирально массив 4 на 4 числами от 1 до 16.");
-    Console.WriteLine("Введите число Х от 2 до 10. Будет сформирован массив размерности X*X");
-    int size = Convert.ToInt32(Console.ReadLine());
-    int[,] array = new int[size,size];
-    FillSpiral(array);
-    PrintArrayInt(array);
-    Console.WriteLine();  
-}
-
-void FillSpiral(int[,] array) {
-    int x = 0;
-    int y = 0;
-
-    array[x,y] = 1;
-    for (int counter = 2; counter < array.GetLength(0)*array.GetLength(1)+1; counter++) {
-        if (StepRight(array, x, y) == true) {
-            y++;
-            array[x,y] = counter;
-        } else if (StepDown(array, x, y) == true) {
-            x++;
-            array[x,y] = counter;
-        } else if (StepLeft(array, x, y) == true) {
-            y--;
-            array[x,y] = counter;
-        } else {
-            array[x - 1, y] = counter;
-            x--;
-            while (array[x - 1, y] == 0) {
-                x--;
-                counter++;
-                array[x,y] = counter;
-            }
-        }
-    }    
-}
-
-bool StepRight(int[,]array, int x, int y) {
-    if (y + 1 < array.GetLength(1) && array[x, y + 1] == 0) return true;
-    else return false;
-}
-
-bool StepDown(int[,]array, int x, int y) {
-    if (x + 1 < array.GetLength(0) && array[x + 1, y] == 0) return true;
-    else return false;
-}
-
-bool StepLeft(int[,]array, int x, int y) {
-    if (y - 1 >= 0 && array[x, y - 1] == 0) return true;
-    else return false;
-}
-
-int[] FindSumsOfLines(int[,] array) {
-    int[] sums = new int[array.GetLength(0)];
-    int sum = 0;
-    for (int i = 0; i < array.GetLength(0); i++) {
-        sum = 0;
-        for (int j = 0; j < array.GetLength(1); j++) {
-            sum += array[i,j];
-        }
-        sums[i] = sum;
+    while (n > m) {
+        Console.WriteLine("Ошибка! Число N < M");
+        Console.WriteLine("Введите число М от 0 до 100:");
+        m = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите число N от 0 до 100:");
+        n = Convert.ToInt32(Console.ReadLine());       
     }
-    return sums;
+
+    Console.WriteLine("Сумма элементов диапазона равна " + SumOfNumbers(m, n));
+    Console.WriteLine(); 
 }
 
-void FindLessLine(int[] array) {
-    int min = array[0];
-    int position = 0;
-    for (int i = 1;i < array.Length; i++ ) {
-        if (array[i] < min) {
-            min = array[i];
-            position = i;
-        }            
-    }
-    Console.WriteLine($"Наименьшая сумма элементов в строке №{position + 1} = {array[position]}");
-}
-
-int[,] FillArray(int[,] array) {
-    //Заполняем двумерный массив случайными числами от -10 до 10
-    Random rand = new Random();
-    for (int i = 0; i < array.GetLength(0); i++) {
-        for (int j = 0; j < array.GetLength(1); j++) {
-            array[i,j] = rand.Next(0,21) - 10;
-        }
+void Zadacha68() {
+    Console.WriteLine("Задача 68. Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.");
+    
+    Console.WriteLine("Введите число М от 0 до 4:");
+    int m = Convert.ToInt32(Console.ReadLine());
+    while (m < 0 || m > 4) {
+        Console.WriteLine("Ошибка! Введите число М от 0 до 4:");
+        m = Convert.ToInt32(Console.ReadLine());
     }
     
-    return array;
-}
-
-void PrintArrayInt(int[,] array) {
-    Console.WriteLine("Выводим массив:");
-    for (int i = 0; i < array.GetLength(0); i++) {
-        for (int j = 0; j < array.GetLength(1); j++) {
-            Console.Write(array[i,j] + "\t");
-        }
-        Console.WriteLine();
-    }  
-}
-
-int[,] SortMaxToMin(int[,] array) {
-    int[] line = new int[array.GetLength(1)];
-    for (int i = 0; i < array.GetLength(0); i++) {
-        for (int j = 0; j < array.GetLength(1); j++) {
-            line[j] = array[i,j];
-        }
-        SortLine(line);
-        for (int j = 0; j < array.GetLength(1); j++) {
-            array[i,j] = line[j];
-        }
+    Console.WriteLine("Введите число N от 0 до 4:");
+    int n = Convert.ToInt32(Console.ReadLine());
+    while (n < 0 || n > 4) {
+        Console.WriteLine("Ошибка! Введите число N от 0 до 4:");
+        n = Convert.ToInt32(Console.ReadLine());
     }
-    return array;
+
+    Console.WriteLine("Значение функции Аккермана = " + A(m, n));
 }
 
-int[] SortLine(int[] array) {
-    int max = array[0];
-    int temp = array[0];
-    int position = 0;
-    for (int i = 0; i < array.Length; i++) {
-        max = array[i];
-        temp = array[i];
-        position = i;
-        for (int j = i + 1;j < array.Length; j++ ) {
-            if (array[j] > max) {
-                max = array[j];
-                position = j;
-            }            
-        }
-        array[i] = max;
-        array[position] = temp;
-    }
-    return array;
+int A(int m, int n) {
+    if (m == 0) return n++;
+    if (m > 0 && n == 0) return A(m - 1, 1);
+    if (m > 0 && n > 0) return A(m - 1, A(m, n - 1));
+    return A(m, n);
 }
 
-Zadacha54();
-Zadacha56();
-Zadacha58();
+void PrintNumbers(int m, int n) {
+    if (m > n) return;
+    if (m%3 == 0) {
+        Console.Write(m + " ");
+        m = m + 3;
+    } else m++;
+    PrintNumbers(m,n);
+}
+
+int SumOfNumbers(int m, int n, int sum = 0, int counter = 0) {
+    if (m+counter > n) return sum;
+    sum += m + counter;
+    counter++;
+    return SumOfNumbers(m, n, sum, counter);
+}
+
+Zadacha64();
+Zadacha66();
+Zadacha68();
